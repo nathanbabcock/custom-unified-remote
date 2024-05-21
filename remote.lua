b272f865d9e15330e7e7d7037da59f7bcd2f5c99
nbabcock <nathan.r.babcock@gmail.com>
2024-05-21 15:46:08 -0500

    feat: move window left/right + working volume buttons

---
+++ remote.lua
@@ -0,0 +1,29 @@
+local keyboard = libs.keyboard;
+
+
+-- Documentation
+-- http://www.unifiedremote.com/api
+
+-- Keyboard Library
+-- http://www.unifiedremote.com/api/libs/keyboard
+
+
+--@help Move Window left
+actions.moveWindowLeft = function ()
+	keyboard.stroke("win", "shift", "left");
+end
+
+
+--@help Move Window right
+actions.moveWindowRight = function ()
+	keyboard.stroke("win", "shift", "right");
+end
+
+--@help Lower system volume
+actions.volume_down = function()
+	keyboard.press("volumedown");
+end
+--@help Raise system volume
+actions.volume_up = function()
+	keyboard.press("volumeup");
+end
